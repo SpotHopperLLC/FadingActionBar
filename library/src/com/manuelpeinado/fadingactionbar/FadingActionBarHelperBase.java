5cdf099042bafecfa11bfa5a19a3878c1f65744a
Marius Volkhart <Marius@volkhart.com>
2014-11-17 12:03:17 -0600

    Make compile after the cherry pick

--- library/src/com/manuelpeinado/fadingactionbar/FadingActionBarHelperBase.java
+++ library/src/com/manuelpeinado/fadingactionbar/FadingActionBarHelperBase.java
@@ -24,6 +24,7 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.MeasureSpec;
 import android.view.ViewGroup;
